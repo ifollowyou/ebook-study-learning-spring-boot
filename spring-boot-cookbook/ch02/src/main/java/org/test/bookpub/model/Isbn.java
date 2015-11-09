0fa387e79f84144c064efb474eb0fc0ba8490a2c
xman <kingsmiler@163.com>
2015-11-09 22:21:29 +0800

    update ch02 isbn

---
+++ spring-boot-cookbook/ch02/src/main/java/org/test/bookpub/model/Isbn.java
@@ -0,0 +1,14 @@
+package org.test.bookpub.model;
+
+
+public class Isbn {
+    private String isbn;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
